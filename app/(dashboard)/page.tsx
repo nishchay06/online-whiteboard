44cf3aebc992a54fa60c922ce92c6a1ae2821b36
Nishchay <nishchay.mago1290@gmail.com>
2024-03-14 22:40:52 +0530

    basic dashboard structure

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,11 @@
+
+const DashboardPage = () => {
+  return (
+    <>
+    <div>Dashboard Root Page</div>
+    <div>This is a screen for authenticated users only</div>
+    </>
+  )
+}
+
+export default DashboardPage
