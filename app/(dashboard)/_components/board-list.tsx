adb5211197eb6196847476a037708298e20375ef
Nishchay <nishchay.mago1290@gmail.com>
2024-03-16 17:02:56 +0530

    added delete board functionality

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -28,6 +28,8 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10'>
           <NewBoardButton orgId={orgId} disabled/>
           <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
         </div>
       </div>
     )
