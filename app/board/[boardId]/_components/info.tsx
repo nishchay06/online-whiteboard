a1281c614dad8c1db0d8848c246965af562ceffd
Nishchay <nishchay.mago1290@gmail.com>
2024-03-17 23:43:04 +0530

    added basic canvas skeleton

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -5,3 +5,9 @@ export const Info = () => {
     </div>
   )
 }
+
+Info.Skeleton = function InfoSkeleton() {
+  return (
+    <div className='absolute top-2 left-2 w-[300px] bg-white rounded-md px-1.5 h-12 flex items-center shadow-md' />
+  )
+}
